70643e3b3b9cb27e0f078e68ff49be40f6611e2e
xXZOFXx <ricardoyael15@gmail.com>
2020-04-22 11:43:05 -0500

    vistas completadas

--- app/src/main/java/com/example/ubicacion/MapsActivity.kt
+++ app/src/main/java/com/example/ubicacion/MapsActivity.kt
@@ -99,35 +99,9 @@ class MapsActivity : AppCompatActivity(), OnMapReadyCallback, GoogleMap.OnMarker
 
 
 
-    fun sendPostRequest(userName:String, password:String) {
 
-        var reqParam = URLEncoder.encode("username", "UTF-8") + "=" + URLEncoder.encode(userName, "UTF-8")
-        reqParam += "&" + URLEncoder.encode("password", "UTF-8") + "=" + URLEncoder.encode(password, "UTF-8")
-        val mURL = URL("<Your API Link>")
 
-        with(mURL.openConnection() as HttpURLConnection) {
-            // optional default is GET
-            requestMethod = "POST"
 
-            val wr = OutputStreamWriter(getOutputStream());
-            wr.write(reqParam);
-            wr.flush();
-
-            println("URL : $url")
-            println("Response Code : $responseCode")
-
-            BufferedReader(InputStreamReader(inputStream)).use {
-                val response = StringBuffer()
-
-                var inputLine = it.readLine()
-                while (inputLine != null) {
-                    response.append(inputLine)
-                    inputLine = it.readLine()
-                }
-                println("Response : $response")
-            }
-        }
-    }
 
 
 
---
+++ app/src/main/java/com/example/ubicacion/data/model/LoggedInUser.kt
@@ -0,0 +1,9 @@
+package com.example.ubicacion.data.model
+
+/**
+ * Data class that captures user information for logged in users retrieved from LoginRepository
+ */
+data class LoggedInUser(
+    val userId: String,
+    val displayName: String
+)
